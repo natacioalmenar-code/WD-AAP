b3b3ea2421683d23ef4f5b77c50c55247cbc2bbf
natacioalmenar-code <natacioalmenar@gmail.com>
2025-12-13 09:28:05 +0100

    Update firebase.ts

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,1 +1,18 @@
+// firebase/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AQUÍ_EL_TEU_API_KEY",
+  authDomain: "AQUÍ_EL_TEU_AUTH_DOMAIN",
+  projectId: "AQUÍ_EL_TEU_PROJECT_ID",
+  storageBucket: "AQUÍ_EL_TEU_STORAGE_BUCKET",
+  messagingSenderId: "AQUÍ_EL_TEU_MESSAGING_ID",
+  appId: "AQUÍ_EL_TEU_APP_ID",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
 
