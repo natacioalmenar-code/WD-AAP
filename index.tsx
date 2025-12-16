98bdb33399416e9874fba8218ddbbcb261833985
natacioalmenar-code <natacioalmenar@gmail.com>
2025-12-16 15:27:29 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-import { AppProvider } from "./context/AppContext";
-import { ErrorBoundary } from "./components/ErrorBoundary";
-
 const root = document.getElementById("root");
-
-if (!root) throw new Error("Missing #root element");
+if (!root) throw new Error("No s'ha trobat #root");
 
 createRoot(root).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <AppProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AppProvider>
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
