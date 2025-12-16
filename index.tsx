09b367a17066ebd711b260d4bd66ff73e2bc0561
natacioalmenar-code <natacioalmenar@gmail.com>
2025-12-16 13:48:05 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import { AppProvider } from "./context/AppContext";
+
 const root = document.getElementById("root");
-if (!root) throw new Error("No s'ha trobat #root");
+
+if (!root) throw new Error("Missing #root element");
 
 createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </HashRouter>
   </React.StrictMode>
 );
